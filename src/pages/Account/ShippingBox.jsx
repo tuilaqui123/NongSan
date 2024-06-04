a1818c21d73b030fe033da3bb17da43beb832c23
trannhutphat23 <trannhutphattv@gmail.com>
2024-06-04 23:40:38 +0700

    update order shipping

--- src/pages/Account/ShippingBox.jsx
+++ src/pages/Account/ShippingBox.jsx
@@ -1,39 +1,46 @@
 import React from "react";
 import itemImg from '../../assets/image/item.png'
 
-const ShippingBox = () => {
+const ShippingBox = ({orderId, orderStatus, name, phone, address, total, items}) => {
+    const formatNumber = (number) => {
+        return new Intl.NumberFormat('de-DE').format(number);
+    }
     return (
         <div className="w-full h-auto border border-gray-500 bg-gray-50 rounded-md p-5">
             {/* mã đơn hàng / thông tin (địa chỉ, tên, đt,..), hình thức thanh toán, trang thái thanh toán,
                 trạng thái đơn hàng, danh sách sản phẩm, dự kiến giao hàng,
              */}
-            <p className="w-full flex justify-between text-lg font-medium">Mã đơn hàng: <span>0923</span></p>
+            <p className="w-full flex justify-between text-lg font-medium">Mã đơn hàng: <span>{orderId}</span></p>
             <p className="w-full flex justify-between text-lg font-medium">Hình thức thanh toán: <span>Tiền mặt</span></p>
-            <p className="w-full flex justify-between text-lg font-medium">Trạng thái đơn hàng: <span>Đang chờ xác nhận</span></p>
+            <p className="w-full flex justify-between text-lg font-medium">Trạng thái đơn hàng: <span>{orderStatus}</span></p>
             <div className="bg-gray-200 p-3 rounded-md my-3">
                 <p className="text-xl font-bold">Thông tin đặt hàng</p>
-                <p className="w-full flex justify-between font-medium">Họ tên: <span>0923</span></p>
-                <p className="w-full flex justify-between font-medium">Số điện thoại: <span>0923</span></p>
-                <p className="w-full flex justify-between font-medium">Địa chỉ: <span>0923</span></p>
+                <p className="w-full flex justify-between font-medium">Họ tên: <span>{name}</span></p>
+                <p className="w-full flex justify-between font-medium">Số điện thoại: <span>{phone}</span></p>
+                <p className="w-full flex justify-between font-medium">Địa chỉ: <span>{address}</span></p>
             </div>
             <div className="bg-gray-200 p-3 rounded-md">
                 <div className="flex flex-row items-center justify-between mb-5">
                     <p className="text-xl font-bold">Thông tin giỏ hàng</p>
-                    <p className="text-xl font-bold text-[#69a737]"><i class="fa-solid fa-money-bill"></i> 100.000đ</p>
+                    <p className="text-xl font-bold text-[#69a737]"><i class="fa-solid fa-money-bill"></i> {formatNumber(total)}đ</p>
                 </div>
-                <div className="flex flex-row gap-3">
-                    <img
-                        src={itemImg}
-                        className="w-[80px] border border-[#3e3e3e] p-2 rounded-md"
-                    />
-                    <div className="w-full flex flex-row items-start justify-between">
-                        <div>
-                            <p className="text-lg font-bold">Rau xà lách</p>
-                            <p>Số lượng: 6</p>
+                {items.map((ele) => {
+                    return (
+                        <div className="flex flex-row gap-3 mb-5">
+                            <img
+                                src={ele.item.image}
+                                className="w-[80px] border border-[#3e3e3e] p-2 rounded-md"
+                            />
+                            <div className="w-full flex flex-row items-start justify-between">
+                                <div>
+                                    <p className="text-lg font-bold">{ele.item.name}</p>
+                                    <p>Số lượng: {ele.amount}</p>
+                                </div>
+                                <p className="text-lg font-bold">{formatNumber(ele.item.price)}đ</p>
+                            </div>
                         </div>
-                        <p className="text-lg font-bold">40.000đ</p>
-                    </div>
-                </div>
+                    )
+                })}
             </div>
         </div>
     )
--- src/pages/Farm/Farm.jsx
+++ src/pages/Farm/Farm.jsx
@@ -53,7 +53,7 @@ const Farm = () => {
                         value={preFarm}
                     />
 
-                    <div className="w-full grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-5 md:mt-10">
+                    <div className="w-full grid grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 md:mt-10">
                         {farms.map((value, index) => {
                             return (
                                 <FarmBox
